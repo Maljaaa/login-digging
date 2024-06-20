ca9a11b27b556e2ad8e22d03ce6771119beb38b6
Maljaaa <Maljaaa@github.com>
2024-06-20 21:21:51 +0900

    feat: setting

---
+++ src/main/java/indi/login/login/domain/User.java
@@ -0,0 +1,35 @@
+package indi.login.login.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.GenerationType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(schema= "git", name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId;
+
+    @Column(name = "user_pw", nullable = false)
+    private String userPw;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Enumerated(ORDINAL)
+    private UserRole role;
+
+}
---
+++ src/main/java/indi/login/login/domain/UserRole.java
@@ -0,0 +1,11 @@
+package indi.login.login.domain;
+
+public enum UserRole {
+    ADMIN, USER;
+
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+}
